e5eece27f3d69e3372c71b73ca3ab02bfdcad2b5
Noah M. Jones <noahj@lanl.gov>
2026-04-08 07:50:34 -0600

    Cargo fmt

--- src/commands/status.rs
+++ src/commands/status.rs
@@ -12,7 +12,7 @@ pub struct StatusArgs {
 
     /// Number of event entries to output, 10 by default, pass -1 to get all event entries.
     #[arg(short = 'e', default_value_t = 10)]
-    event_count: usize
+    event_count: usize,
 }
 
 pub fn status(cli: &Cli, args: &StatusArgs) -> HandledResult<()> {
@@ -96,15 +96,15 @@ pub fn status(cli: &Cli, args: &StatusArgs) -> HandledResult<()> {
     } else {
         println!(", {} (deactivated)", dd);
     }
-    if !args.exclude_normal{
+    if !args.exclude_normal {
         print!("Events: ");
-        if cluster.events.is_empty(){
+        if cluster.events.is_empty() {
             println!("NULL");
         } else {
             println!();
             for (i, e) in cluster.events.iter().enumerate() {
-                if i == args.event_count{
-                    break
+                if i == args.event_count {
+                    break;
                 }
                 println!("{}", e.syslog_print())
             }
